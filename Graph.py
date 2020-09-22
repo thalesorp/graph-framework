66d572f4821ff9a42b32fdb75bc549fe7b948988
ThalesORP <ThalesORP@gmail.com>
2020-09-22 17:49:53 -0300

    Making basic AdjacencyListGraph methods

--- Graph.py
+++ Graph.py
@@ -25,38 +25,33 @@ class AdjacencyListGraph:
         self.directed = directed
         self.undirected = not directed
 
-        self.m = [[0 for _ in range(self.n_vertices)] for _ in range(self.n_vertices)]
-        print(self.m)
+        self.adjacency_list = [[] for _ in range(n_vertices)]
 
     def insert_edge(self, u, v):
         ''' Inserts an edge from vertex u to vertex v.'''
 
-        self.m[u][v] = 1
-        if self.undirected:
-            self.m[v][u] = 1
+        self.adjacency_list[u].append(v)
 
     def remove_edge(self, u, v):
         ''' Removes an edge from vertex u to vertex v.'''
 
-        self.m[u][v] = 0
-        if self.undirected:
-            self.m[v][u] = 0
+        self.adjacency_list[u].remove(v)
 
     def has_edge(self, u, v):
         ''' Checks if there's a edge from vertex u to vertex v.'''
 
-        if self.m[u][v] == 1:
+        if v in self.adjacency_list[u]:
             return True
         return False
 
     def show_graph(self):
-        ''' Print the adjacency matrix.'''
+        ''' Prints the whole structure.'''
 
-        for row in self.m:
-            for value in row:
-                print(value, end =" ")
+        for i in range(len(self.adjacency_list)):
+            print(i, end=": ")
+            for adjacency in self.adjacency_list[i]:
+                print(adjacency, end =" ")
             print()
-
 
 class AdjacencyMatrixGraph:
 
@@ -99,4 +94,3 @@ class AdjacencyMatrixGraph:
             for value in row:
                 print(value, end =" ")
             print()
-
