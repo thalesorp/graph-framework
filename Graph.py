338bb2f3b859fe18c443c2ad6cd7d5d9c45345de
ThalesORP <ThalesORP@gmail.com>
2020-09-29 17:27:10 -0300

    Now using dict to store data

--- Graph.py
+++ Graph.py
@@ -15,16 +15,23 @@
 
 ''' This module contains two graph abstract data type: adjacency list and adjacency matrix'''
 
+LOWERCASE_ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 class AdjacencyListGraph:
 
     ''' A graph abstract data type using adjacency list representation'''
 
-    def __init__(self, directed, wheighted, vertex_quantity=None):
+    def __init__(self, directed, weighted, vertex_quantity=None):
         ''' "directed" and "wheight" MUST be boolean. '''
 
         self.undirected = not directed
-        self.wheighted = wheighted
+        self.weighted = weighted
         self.vertex_quantity = None
+
+        self.edges = dict()
+
+        if self.weighted:
+            self.weights = dict()
 
         if vertex_quantity is not None:
             self.set_vertex_quantity(vertex_quantity)
@@ -35,147 +42,135 @@ class AdjacencyListGraph:
 
         self.vertex_quantity = vertex_quantity
 
-        if self.wheighted:
-            self.edges = [dict() for _ in range(self.vertex_quantity)]
-        else:
-            self.edges = [list() for _ in range(self.vertex_quantity)]
+        for i in range(vertex_quantity):
+            self.edges[LOWERCASE_ALPHABET[i]] = []
 
     def insert_edge(self, origin, destination, wheight=None):
         ''' Inserts an edge from vertex origin to vertex destination.'''
 
-        if self.wheighted:
-            self.edges[origin][destination] = wheight
-
-            if self.undirected:
-                self.edges[destination][origin] = wheight
-
-        else:
-            self.edges[origin].append(destination)
-
-            if self.undirected:
-                self.edges[destination].append(origin)
-
-    def remove_edge(self, origin, destination):
-        ''' Removes an edge from vertex u to vertex v.'''
-
-        if self.wheighted:
-            del self.edges[origin][destination]
-
-            if self.undirected:
-                del self.edges[destination][origin]
-
-        else:
-            self.edges[origin].remove(destination)
-
-            if self.undirected:
-                self.edges[destination].remove(origin)
+        self.edges[origin].append(destination)
+        if self.weighted:
+            self.weights[origin].append(wheight)
 
     def has_edge(self, origin, destination):
         ''' Checks if there's a edge from vertex origin to vertex destination.'''
 
-        if destination in self.edges[origin]:
+        if (origin in self.edges) and (destination in self.edges[origin]):
             return True
         return False
 
-    def delete(self):
-        foo = True
+    def __str__(self):
+        ''' Returns a string with the whole structure ready to be printed.'''
+
+        result = "{\n"
+
+        for vertex_origin in self.edges:
+            result += "  " + str(vertex_origin) + " = "
+
+            for i in range(len(self.edges[vertex_origin])):
+                result += str(self.edges[vertex_origin][i])
+                if self.weighted:
+                    result += ":" + str(self.weights[vertex_origin][i])
+                result += " "
+
+            result += "\n"
+
+        result += "}"
+
+        return result
 
     # Utils
-    def show_graph(self):
-        ''' Prints the whole structure.'''
+    def get_adjacent_vertices(self, vertex):
+        ''' Return a list containing all adjacent vertices of "vertex".'''
 
-        for i in range(len(self.edges)):
-            print(i, end=" = ")
-            if self.wheighted:
-                print(self.edges[i], end =" ")
-            else:
-                for adjacency in self.edges[i]:
-                    print(adjacency, end =" ")
-            print()
+        adjacent_vertices = self.edges[vertex]
 
-    def graph_from_file(self, file_path):
-        ''' Receives a file path and read it as follows:
-        "VERTEX_QUANTITY
-        EDGE_QUANTITY"
-        This meaning that, the two first lines is the vertices quantity and edge quantity,
-        respectively. After that, it contains EDGE_QUANTITY lines as follows:
-        "ORIGIN DESTINATION WHEIGHT"'''
+        for origin_vertex, destination_vertex_list in self.edges.items():
+            if vertex in destination_vertex_list:
+                adjacent_vertices.append(origin_vertex)
 
-        graph_file = open(file_path,'r')
-
-        self.set_vertex_quantity(int(graph_file.readline()))
-        edge_quantity = int(graph_file.readline())
-
-        for line in graph_file:
-            origin, destination, wheight = line.split()
-            self.insert_edge(int(origin), int(destination), float(wheight))
-
-        graph_file.close()
+        return adjacent_vertices
 
     # Useful algorithms
-    def depth_first_search_util(self, vertex, visited):
+    def depth_first_search(self, visited, vertex):
+        ''' Depth first search algorithm.'''
+
         # Marking the current vertex as visited
         visited[vertex] = True
-        print(vertex, end = ' ')
 
-        for i in range(len(self.edges)):
-            if visited[i] is False:
-                self.depth_first_search_util(i, visited)
+        print(vertex, end=",")
 
-    def depth_first_search(self, vertex):
-        # Marking all vertices as not visited
-        #visited = [False] * self.vertex_quantity
-        visited = [[False] for _ in range(self.vertex_quantity)]
+        adjacent_vertices = self.get_adjacent_vertices(vertex)
 
-        self.depth_first_search_util(vertex, visited)
+        for adjacent_vertex in adjacent_vertices:
+            if visited[adjacent_vertex] is False:
+                self.depth_first_search(visited, adjacent_vertex)
 
+    def connected_components(self):
+        ''' Count how many connected components there is.'''
 
+        connected_components_quantity = 0
 
+        # Creating a dictionary with "False" as value for each vertex (which is used as key)
+        visited = {key: False for key in self.edges.keys()}
 
+        for origin_vertex in self.edges.keys():
 
+            if visited[origin_vertex] is False:
+                self.depth_first_search(visited, origin_vertex)
+                connected_components_quantity += 1
+                print()
 
+        print(connected_components_quantity, "connected components", end="\n")
 
+# URI problems
+def uri_1081():
+    ''' URI 1082: https://www.urionlinejudge.com.br/judge/en/problems/view/1082
+    Input:
+    "N" number of tests
+    "V E" number of vertices and edges
+    "O D" lines, each is a connection between two edges: origin and destination
+    '''
 
+    number_of_tests = input()
 
+    for i in range(int(number_of_tests)):
 
+        vertex_quantity, edge_quantity = input().split()
 
-class AdjacencyMatrixGraph:
+        directed = True
+        wheighted = False
+        graph = AdjacencyListGraph(directed, wheighted, int(vertex_quantity))
 
-    ''' A graph abstract data type using adjacency matrix representation'''
+        for _ in range(int(edge_quantity)):
+            origin, destination = input().split()
+            graph.insert_edge(origin, destination)
 
-    def __init__(self, n_vertices, directed):
+        print("Case #" + str(i+1) + ":")
+        graph.connected_components()
 
-        self.vertex_quantity = int(n_vertices)
-        self.undirected = not directed
+def uri_1774():
+    ''' URI 1774: https://www.urionlinejudge.com.br/judge/en/problems/view/1774
+    '''
+    print("URI 1774")
 
-        self.m = [[0 for _ in range(self.vertex_quantity)] for _ in range(self.vertex_quantity)]
-        print(self.m)
+# DEBUG
+def debug():
 
-    def insert_edge(self, u, v):
-        ''' Inserts an edge from vertex u to vertex v.'''
+    print("DEBUGGING...")
 
-        self.m[u][v] = 1
-        if self.undirected:
-            self.m[v][u] = 1
+    directed = True
+    wheighted = True
+    g = AdjacencyListGraph(directed, wheighted)
 
-    def remove_edge(self, u, v):
-        ''' Removes an edge from vertex u to vertex v.'''
+    g.insert_edge("a", "b", 21)
+    g.insert_edge("a", "c", 35)
+    g.insert_edge("b", "c", 23)
 
-        self.m[u][v] = 0
-        if self.undirected:
-            self.m[v][u] = 0
+    print(g.has_edge("a", "c"))
+    print(g.has_edge("b", "c"))
 
-    def has_edge(self, u, v):
-        ''' Checks if there's a edge from vertex u to vertex v.'''
+    print(g)
 
-        if self.m[u][v] == 1:
-            return True
-        return False
-
-    def show_graph(self):
-        ''' Print the adjacency matrix.'''
-
-        for row in self.m:
-            for value in row:
-                print(value, end =" ")
-            print()
+uri_1081()
+uri_1774()
